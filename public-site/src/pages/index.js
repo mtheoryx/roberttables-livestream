c55c53f49a3e9a67b1613fbab48867d43164571c
David Poindexter <drpoindexter@gmail.com>
2019-05-25 16:06:53 -0400

    Add schedule temp data, component, and display on index page

--- public-site/src/pages/index.js
+++ public-site/src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import Schedule from "../components/Schedule/Schedule"
 
 const IndexPage = () => (
   <Layout>
     <h1>Welcome to the High Table.</h1>
     <h3>No business on company grounds.</h3>
     <p>It wasn't just a dog.</p>
+    <Schedule />
   </Layout>
 )
 
