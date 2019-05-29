66f4e0c0d5d860ae6d65d3e9e845898f4ec4ceaa
David Poindexter <drpoindexter@gmail.com>
2019-05-28 20:57:07 -0400

    Restyle with styled-componets

--- public-site/src/components/layout.js
+++ public-site/src/components/layout.js
@@ -8,9 +8,18 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
 
-import Header from "./header"
+import Header from "./Header"
+import Footer from "./Footer"
 import "./layout.css"
+
+const StyledContainer = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0px 1.0875rem 1.45rem;
+  padding-top: 0;
+`
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -33,21 +42,10 @@ const Layout = ({ children }) => (
           siteTitle={data.site.siteMetadata.title}
           navLinks={data.site.siteMetadata.navLinks}
         />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <StyledContainer>
           <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+          <Footer />
+        </StyledContainer>
       </>
     )}
   />
