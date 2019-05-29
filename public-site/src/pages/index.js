6ee30905fc62456c6ffe5631355522a6fb8463ea
David Poindexter <drpoindexter@gmail.com>
2019-05-28 20:56:42 -0400

    CHange schedule import path

--- public-site/src/pages/index.js
+++ public-site/src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import Schedule from "../components/Schedule/Schedule"
+import Schedule from "../components/Schedule"
 
 const IndexPage = () => (
   <Layout>
